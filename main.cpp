4cf51c16d0132f2e01ff8e4e18732ff89d96852f
Marcel Baur <marcel.baur@tawny.ai>
2020-12-06 19:23:01 +0100

    Two fields in main function

--- main.cpp
+++ main.cpp
@@ -5,23 +5,23 @@
 using namespace std::chrono;
 
 int main() {
+    Field genField;
     Field field;
     // GENERATOR
-    field.generate(10,10);
-
+    genField.generate(10,10);
 
     // SOLVER
-//    field.generateFromFile("../inputs/tents-8x8-t1.txt");
-//    cout << "Solving...\n";
-//    int begin = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-//    bool sol = field.solve();
-//    int end = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-//    cout << '\n';
-//    field.printField();
-//    cout << "Solved!\n";
-//    cout << "Solution: " << sol << '\n';
-//    int time = end - begin;
-//    cout << time << '\n';
+    field.generateFromFile("../inputs/tents-8x8-t1.txt");
+    cout << "Solving...\n";
+    int begin = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    bool sol = field.solve();
+    int end = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    cout << '\n';
+    field.printField();
+    cout << "Solved!\n";
+    cout << "Solution: " << sol << '\n';
+    int time = end - begin;
+    cout << time << '\n';
 
 
 
